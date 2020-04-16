2b0a703d00bc209ebcd386990c88c82a07d3a231
Parkchangun <qkrcksrjs@gmail.com>
2020-04-16 22:13:44 +0900

    commit

--- pointer_test.c
+++ pointer_test.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 
 void pointer(){
-    int num = 5;
-    int *pointer1 = &num;
-    int **pointer2 = &pointer1;
-
-    printf("%d\n%d", *pointer2, **pointer2); //*pointer2 = pointer2가 가리키는 주소값
-                                             //**pointer2 = pointer2가 가리키는 변수의 주소값
+    int array[] = {0 ,10 ,2};
+    int *point = array;
+    printf("%d", point[1]);
 }
 
 void main(){
