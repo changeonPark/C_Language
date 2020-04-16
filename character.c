2bb12b813a324d5b1a4afc7acbe3bb80f5655b77
Parkchangun <qkrcksrjs@gmail.com>
2020-04-16 22:49:50 +0900

    commit

---
+++ character.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+//아스키 코드
+/*
+    0~9 = 48~57
+    'A'~'Z' = 65~90
+    'a'~'z' = 97~122
+*/
+void character01(){
+    char x = 122;
+    printf("%c\n", x);
+}
+
+
+
+
+
+void main(){
+    character01();
+}
