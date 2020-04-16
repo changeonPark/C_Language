e935e761783253d44011ee2e00b1f5f53a27f3df
Parkchangun <qkrcksrjs@gmail.com>
2020-04-16 23:10:05 +0900

    commit

--- character.c
+++ character.c
@@ -4,10 +4,13 @@
     0~9 = 48~57
     'A'~'Z' = 65~90
     'a'~'z' = 97~122
+    줄바꿈이나 공백도 아스키 코드로 관리.
 */
 /*
     문자열 처리시 버퍼의 개념 많이 사용.
     Buffer = 임시적으로 특정한 데이터 저장을 위해 사용.
+    컴퓨터가 한번에 처리 가능한 양은 한정적이므로 Buffer에 담아서 처리.
+
 */
 void character01(){
     char x = 122;
@@ -19,8 +22,18 @@ void character02(){
     printf("%c\n", x);
 }
 
+void buffer(){
+    int num; char ch;
+    scanf("%d", &num);
+    printf("%d\n", num);
+    int temp;
+    //while문 반복 동안 파일의 끝 or 개행 문자일 시 입력 중지 --> 버퍼 비움.
+    while((temp = getchar()) != EOF && temp != '\n') {}
+    scanf("%c", &ch);
+    printf("%c\n", ch);
+}
 void main(){
     character01();
     character02();
-
+    buffer();
 }
