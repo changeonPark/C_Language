490b099ed8e84f876cf32e6c4b91024d916b5fb4
Parkchangun <qkrcksrjs@gmail.com>
2020-04-19 22:37:36 +0900

    commit

--- fileio.c
+++ fileio.c
@@ -4,6 +4,27 @@
 
 
 
+struct Family{
+    char name[20];
+    int birth;
+};
+
+void famFunc(){
+    int num, sum = 0;
+    FILE *fp = fopen("input.txt", "r");
+    fscanf(fp, "%d", &num);
+    struct Family *fam = malloc(sizeof(fam) * num);
+    for(int i = 0; i < num; i++){
+        fscanf(fp, "%s %d", &((fam + i)->name), &((fam + i)->birth) ); 
+    }
+    free(fam);
+    fclose(fp);
+    for(int i = 0; i < num; i++){
+        sum += (fam + i)->birth; 
+    }
+    printf("average = %.2f\n", (double)sum / num);
+}
+
 
 void print(){
     char s[20] = "file test";
@@ -14,5 +35,5 @@ void print(){
 
 
 void main(){
-    print();
+    famFunc();
 }
